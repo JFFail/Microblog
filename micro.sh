7d297649694ac1fddaf031692b4d7a3e6242ebff
JFFail <jjfabry@gmail.com>
2015-07-28 17:39:04 +0000

    Added a 150 character length limit.

--- micro.sh
+++ micro.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env zsh
-date_val=$(date)
-text="<p>$1<\/p>\n<p class=\"footnote\">\n<time class=\"\">$date_val<\/time>\n"
-sed -i "s/<\!-- Watermark -->/<\!-- Watermark -->\n$text/g" ~/micro/index.html
-cp ~/micro/index.html ~/blog/public/micro/
-cp ~/micro/index.html ~/html/micro/
+length=$(echo $1 | wc -c)
+if [[ $length -gt 150 ]]; then
+    echo "$length characters is too many!"
+else
+    date_val=$(date)
+    text="<p>$1<\/p>\n<p class=\"footnote\">\n<time class=\"\">$date_val<\/time>\n"
+    sed -i "s/<\!-- Watermark -->/<\!-- Watermark -->\n$text/g" ~/micro/index.html
+    cp ~/micro/index.html ~/blog/public/micro/
+    cp ~/micro/index.html ~/html/micro/
+fi
