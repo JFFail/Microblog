beb25750b7c1812465a0f9cd71a24045d5ddfa84
JFFail <jjfabry@gmail.com>
2015-07-30 18:11:10 +0000

    Updated the class on the timestamp to indent it.

--- micro.sh
+++ micro.sh
@@ -4,7 +4,7 @@ if [[ $length -gt 150 ]]; then
     echo "$length characters is too many!"
 else
     date_val=$(date)
-    text="<p>$1<\/p>\n<p class=\"footnote\">\n<time class=\"\">$date_val<\/time>\n"
+    text="<p>$1<\/p>\n<p class=\"footnote\">\n<time class=\"blogtime\">$date_val<\/time>\n"
     sed -i "s/<\!-- Watermark -->/<\!-- Watermark -->\n$text/g" ~/micro/index.html
     cp ~/micro/index.html ~/blog/static/micro/
     cp ~/micro/index.html ~/html/micro/
