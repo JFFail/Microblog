680b3a108627c6adcbb64e3014bd7fa832eb11be
JFFail <jjfabry@gmail.com>
2015-08-03 22:42:37 +0000

    Added some options for URLs.

--- micro.sh
+++ micro.sh
@@ -4,7 +4,12 @@ if [[ $length -gt 150 ]]; then
     echo "$length characters is too many!"
 else
     date_val=$(date)
-    text="<p>$1<\/p>\n<p class=\"footnote\">\n<time class=\"blogtime\">$date_val<\/time>\n"
+    if [[ ! -z $2 && ! -z $3 ]]; then
+        value="$1 > <a href=\"$2\">$3<\/a>"
+    else
+        value=$1
+    fi
+    text="<p>$value<\/p>\n<p class=\"footnote\">\n<time class=\"blogtime\">$date_val<\/time>\n"
     sed -i "s/<\!-- Watermark -->/<\!-- Watermark -->\n$text/g" ~/micro/index.html
     cp ~/micro/index.html ~/blog/static/micro/
     cp ~/micro/index.html ~/html/micro/
